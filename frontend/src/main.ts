56e3ca537666c48bd619e990504b77be2681f44b
batoripeter <batorifoto@gmail.com>
2023-08-27 00:36:56 +0200

    update and sending works

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 import axios from "axios";
 import { z } from "zod";
+import cors from 'cors';
 
 const PizzaSchema = z.object ({
   id: z.number(),
@@ -42,6 +43,7 @@ const getPizzas = async () => {
     pizzas = result.data
 }
 
+
 const selectPizza = (id: number) => {
   selectedPizza = pizzas.find(pizza => pizza.id === id) || null
 }
@@ -65,13 +67,12 @@ const updateOrderWithItem = () => {
       { id: selectedPizza!.id, amount }
     ]
   }
-} 
+}
 
 const deleteItem = () => {
   if(order)
     order.items.splice(order.items.findIndex(item => item.id === selectedPizza!.id),1)
-  } 
-
+         } 
 
 // render
 const renderList = (pizzas: Pizza[]) => {
@@ -90,10 +91,10 @@ const renderSelected = (pizza: Pizza) => {
   const content = `
     <div>
       <h1>${pizza.name}</h1>
-      <p class="bg-red-600">${pizza.toppings}</p>
-      <img src="${pizza.url}" />
-      <input type="number" id="amount"  class="bg-red-500"/>
-      <button id="add" class="bg-red-500">Add to order</button>
+      <p>${pizza.toppings}</p>
+      <img src="${pizza.url}" width="300px"/>
+      <input type="number" id="amount" class="text-black" />
+      <button id="add" >Add to order</button>
     </div>
   `
   document.getElementById("selected")!.innerHTML = content
@@ -107,18 +108,19 @@ const renderOrder = (order: Order) => {
       <h1>Your order</h1>
       ${order.items.map(item => `
         <div class="flex">
-        <p class="bg-red-500">${item.amount} x ${pizzas.find(pizza => pizza.id === item.id)!.name}</p>
-        <button class="bg-red-900" id="remove">Remove from order</button>
+        <p>${item.amount} x ${pizzas.find(pizza => pizza.id === item.id)!.name}</p>
+        <button id="remove" class="mx-3 px-2 bg-red-800">Remove from order</button>
         </div>
-      `)}
-      <input placeholder="Name" id="orderName" class="bg-red-900">
-      <input placeholder="Zip code" id="zipCode" class="bg-red-900">
-      <buttonc lass="bg-red-900">Send order</button>
+      `).join(" ")}
+      <input placeholder="Name" id="orderName" class="text-black" value="${order.name}">
+      <input placeholder="Zip code" class="text-black" id="zipCode" value="${order.zipCode}">
+      <button id="sendOrder">Send order</button>
     </div>
   `
   document.getElementById("order")!.innerHTML = content;
-  (document.getElementById("orderName")as HTMLInputElement).value = order.name;
-  (document.getElementById("zipCode")as HTMLInputElement).value = order.zipCode;
+  (document.getElementById("orderName") as HTMLInputElement).addEventListener("change", personListener);
+  (document.getElementById("zipCode") as HTMLInputElement).addEventListener("change", personListener);
+  (document.getElementById("sendOrder") as HTMLButtonElement).addEventListener("click", sendListener);
   document.querySelectorAll("#remove").forEach((el)=>{
     el.addEventListener("click", deleteListener)
     })
@@ -142,6 +144,12 @@ const changeListener = (event: Event) => {
   updateAmount(+(event.target as HTMLInputElement).value)
 }
 
+const personListener = () => {
+  order!.name = (document.getElementById("orderName") as HTMLInputElement).value
+  order!.zipCode = (document.getElementById("zipCode") as HTMLInputElement).value
+  if (order)
+  renderOrder(order)
+}
 
 const deleteListener = () => {
   deleteItem()
@@ -150,10 +158,41 @@ const deleteListener = () => {
 }
 
 const addListener = () => {
-  updateOrderWithItem()
+ updateOrderWithItem()
   if (order)
     renderOrder(order)
 }
+
+const sendListener = () => {
+  if (order)
+  send(order)
+}
+
+//send
+
+async function send(order:Order) {
+  
+    try {
+      isLoading = true
+      const response = await axios("http://localhost:3333/api/order", {
+        method: "POST", 
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify(order),
+      });
+      isLoading = false
+      const result = await response.data;
+      console.log("Success:", result);
+      (document.getElementById("sendOrder") as HTMLButtonElement).style.display = "none" // :)
+      let message = document.createElement("p");
+      document.getElementById("order")!.appendChild(message);
+      message.style.backgroundColor="red"
+      message.innerHTML = "Order sent, thank you!"
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
 
 init()
